98a9881e7fee2ed0f9a25cf37485d60b62fcf128
khatbahusain <khatba.husain@hotmail.com>
2023-03-16 20:19:49 +0100

    fix tests

--- test/booking_infos.py
+++ test/booking_infos.py
@@ -6,7 +6,6 @@ class BookingInfos:
         str_date: str = None,
         end_date: str = None,
         budget: str = None,
-        confirm: str = None
     ):
         
         self.or_city: str = or_city
@@ -14,4 +13,3 @@ class BookingInfos:
         self.str_date: str = str_date
         self.end_date: str = end_date
         self.budget: str = budget
-        self.confirm: str = confirm
