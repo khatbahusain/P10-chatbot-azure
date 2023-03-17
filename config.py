87ec20c3d54fa12b82e94a3ac5850363427921ab
khatbahusain <khatba.husain@hotmail.com>
2023-03-17 16:28:11 +0100

    fix bot

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 8000
-    APP_ID = os.environ.get("MicrosoftAppId", "d7766af3-19db-40a0-8b90-3b2ec4bccd95")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "9.k8Q~C2~Yut7oNb2y4ZSGRavRpDPYU4ojTMtdkY")
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
     LUIS_APP_ID = os.environ.get("LuisAppId", "672fbb9a-188f-4eda-a279-970645ebe47f")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "702fc50696ab44d9a2bd7afd5b0214ae")
     LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "flymeoc-authoring.cognitiveservices.azure.com")
